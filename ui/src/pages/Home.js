3030affadc77e7194ea17567115f1e030300f169
Clinery1 <clinery8237@gmail.com>
2023-02-23 17:20:18 -0500

    Add a Login and Signup button to the home page

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,8 +1,11 @@
+import {Link as ReactLink} from "react-router-dom";
 import {
     Text,
     Flex,
     Spacer,
     Heading,
+    HStack,
+    Button,
 } from "@chakra-ui/react";
 import Auth from "../utils/auth";
 
@@ -18,6 +21,15 @@ export default () => {
                 <Text align="center">You should start by either making an account or logging in.</Text>
                 <Spacer minHeight="1ch" />
                 <Text align="center">You can also view all of the supply listings by pressing the "Listings" nav button</Text>
+                <Spacer minHeight="1ch" />
+                <HStack>
+                    <Button width="min-content" as={ReactLink} to="/login">
+                        Login
+                    </Button>
+                    <Button width="min-content" as={ReactLink} to="/signup">
+                        Signup
+                    </Button>
+                </HStack>
             </>)}
         </Flex>
     );
